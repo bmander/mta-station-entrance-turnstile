ae1f5c137667f7d38eebdc34b333e35ae54ac489
Brandon Martin-Anderson <badhill@gmail.com>
2013-07-09 20:05:58 -0400

    script to prepopulate station geojson, and the geojson itself

---
+++ get_entrances.py
@@ -0,0 +1,23 @@
+from urllib2 import urlopen
+import csv
+import simplejson as json
+
+fp = urlopen( "http://www.mta.info/developers/data/nyct/subway/StationEntrances.csv" )
+
+rd = csv.reader( fp )
+
+header_row = rd.next()
+hdr = dict(zip(header_row,range(len(header_row))))
+
+features = []
+for row in rd:
+	geometry = {"type":"Point","coordinates":(row[hdr["Longitude"]],row[hdr["Latitude"]])}
+	properties = {}
+	feature = {"type":"Feature","properties":properties,"geometry":geometry}
+
+	features.append( feature )
+
+output = {"type":"FeatureCollection","features":features}
+
+fpout = open("station_entrances.geojson","w")
+fpout.write( json.dumps(output, indent=2) )
