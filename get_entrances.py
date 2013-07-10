cca8fc09b6271276d96cf111f3d3f015c3a0e5f6
Brandon Martin-Anderson <badhill@gmail.com>
2013-07-09 20:13:03 -0400

    populate geojson with metadata about entrances

--- get_entrances.py
+++ get_entrances.py
@@ -12,9 +12,14 @@ hdr = dict(zip(header_row,range(len(header_row))))
 features = []
 for row in rd:
 	geometry = {"type":"Point","coordinates":(row[hdr["Longitude"]],row[hdr["Latitude"]])}
-	properties = {}
-	feature = {"type":"Feature","properties":properties,"geometry":geometry}
 
+	properties={}
+	for i,field in enumerate(header_row):
+		if field=="Latitude" or field=="Longitude":
+			continue
+		properties[field] = row[i]
+
+	feature = {"type":"Feature","properties":properties,"geometry":geometry}
 	features.append( feature )
 
 output = {"type":"FeatureCollection","features":features}
